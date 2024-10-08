106c4145345adacdd9c892b83630840a144a6b81
Luke Arcebal <103071251+LukeBArcebal@users.noreply.github.com>
2024-10-09 03:05:20 +1000

    Added Fading to Lid

--- A Slice of Lunch/Assets/Scripts/HintSystem/Draggable.cs
+++ A Slice of Lunch/Assets/Scripts/HintSystem/Draggable.cs
@@ -5,32 +5,35 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
-    private BoxCollider2D myCollider; //collider of the lid
-    public BoxCollider2D baseCollider; //collider of the base
-    // private int opacity = 255;
+    [SerializeField] private float fadeOutRadius;
+    [SerializeField] private float fadeTime;
     private bool dragging = false; //true if left click is held down flase otherwise
     private Vector3 offset; //mouse position - object position
+    private SpriteRenderer spriteRenderer;
 
-    // Update is called once per frame
-    void Start()
+    IEnumerator Fade()
     {
-        myCollider = GetComponent<BoxCollider2D>();
+        Color c = spriteRenderer.color;
+        for (float alpha = 1f; alpha >= 0; alpha -= 0.05f)
+        {
+            c.a = alpha;
+            spriteRenderer.color = c;
+            yield return new WaitForSeconds(fadeTime);
+        }
+        gameObject.SetActive(false);
     }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
     void Update()
     {
         if (dragging) //If mouse down
         {
             //Apply change in mouse location to object's location ignoring difference in location
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset; 
-        }
-
-        if (myCollider.IsTouching(baseCollider)) //If the lid is not touching the base
-        {
-            Debug.Log("The object is touching");
-            //lower opacity value
-            //apply opacity value
-            //if the opacity is less than , delete lid
-
         }
     }
 
@@ -46,5 +49,12 @@ public class Draggable : MonoBehaviour
     {
         //send false when mouse up
         dragging = false;
+
+        //if the position is fadeOutRadius away from (0,0)
+        if(Vector2.Distance((Vector2)transform.position, Vector2.zero) >= fadeOutRadius)
+        {
+            //fade object (dcrease alpha channel)
+            StartCoroutine(Fade());
+        }
     }
 }
