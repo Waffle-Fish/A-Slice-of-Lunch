d7985a3ddf8f98caa5a1301589b5d904dd50b09f
Felix <fexlixhoang7@gmail.com>
2024-10-10 20:17:54 -0700

    Click and drag is now working

--- A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
+++ A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
@@ -1,51 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.EventSystems;
 
-public class ControlFood : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class ControlFood : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    bool dragging = false;
+    
+    private void OnMouseDown() {
+        dragging = true;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    Ray clickRay;
-
-
-    public void OnPointerDown(PointerEventData eventData) {
-        Debug.Log("OnPointDown");        
-    }
-    public void OnEndDrag(PointerEventData eventData) {
-        Debug.Log("OnEndDrag");
-    }
-    public void OnDrag(PointerEventData eventData) {
-        Vector2 foodPosition = Vector2.zero;
-        foodPosition += eventData.delta;
-        Debug.Log("OnDrag");
-    }
-    public void OnBeginDrag(PointerEventData eventData) {
-        Debug.Log("OnBeginDrag");
-    }
-
-
-    private string DetectObject() {
-            clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(clickRay.origin, clickRay.direction * 100, Color.green, 5f);
-            RaycastHit2D hit2D =  Physics2D.GetRayIntersection(clickRay);
-            if (hit2D.collider != null) {
-                Debug.Log(hit2D.collider.gameObject.name);
-                return hit2D.collider.tag;
-            }
-            return "None";
+    private void Update() {
+        if (dragging) {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0f;
+            transform.position = pos;
         }
-
+    }
+    private void OnMouseUp() {
+        dragging = false;
+    }
 }
