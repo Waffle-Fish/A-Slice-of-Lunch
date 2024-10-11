81e414e41421103ed25b390b82fe87262d18927a
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-10-11 01:01:45 -0700

    update slice and controlFood
    + can't slice and move food at the same time
    + can't slice if starting position is on the food

--- A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
+++ A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class ControlFood : MonoBehaviour
 {
     bool dragging = false;
+    PlayerControls playerControls;
     
+    private void Awake() {
+        playerControls = GameObject.FindWithTag("Player").GetComponent<PlayerControls>();
+    }
+
     private void OnMouseDown() {
+        if (playerControls.IsHoldingKnife) return;
         dragging = true;
     }
+
     private void Update() {
         if (dragging) {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -14,6 +21,7 @@ public class ControlFood : MonoBehaviour
             transform.position = pos;
         }
     }
+
     private void OnMouseUp() {
         dragging = false;
     }
--- A Slice of Lunch/Assets/Scripts/CharacterControls/PlayerControls.cs
+++ A Slice of Lunch/Assets/Scripts/CharacterControls/PlayerControls.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,10 +15,9 @@ public class PlayerControls : MonoBehaviour
 
     [Header("Slice Variables")]
     [SerializeField]
-    private bool isHoldingKnife = false;
-    [SerializeField]
     private GameObject spriteMask;
     private Vector3[] slicePoints = new Vector3[2];
+    public bool IsHoldingKnife /*{ get; private set; }*/ = false;
     readonly private Vector3 CHECK_VECTOR = new Vector3(999999, 999999, 999999);
     List<RaycastHit2D> slicedObjects;
 
@@ -65,8 +65,7 @@ public class PlayerControls : MonoBehaviour
 
     private void DetectLeftClick()
     {
-        if (!isHoldingKnife) return;
-
+        if (!IsHoldingKnife) return;
         if (Mouse.current.leftButton.wasPressedThisFrame) BeginSlice();
         if (Mouse.current.leftButton.wasReleasedThisFrame) FinalizeSlice();
     }
@@ -81,14 +80,19 @@ public class PlayerControls : MonoBehaviour
     }
 
     private void BeginSlice() {
-        if (slicePoints[0] != CHECK_VECTOR) return;
+        // return if slice starts on food
+        clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(clickRay.origin, clickRay.direction * 100, Color.green, 5f);
+        RaycastHit2D hit2D =  Physics2D.GetRayIntersection(clickRay);
+        if (hit2D.collider != null && hit2D.collider.CompareTag("Food")) return;
+
         slicePoints[0] = mouseWorldPosition;
         foreach (var ep in endPoints) ep.SetActive(true);
         endPoints[0].transform.position = new (slicePoints[0].x, slicePoints[0].y, endPointPosZ);
     }
 
     private void FinalizeSlice() {
-        if (slicePoints[1] != CHECK_VECTOR) return;
+        if (slicePoints[0] == CHECK_VECTOR) return;
 
         slicePoints[1] = mouseWorldPosition;
         slicedObjects = Physics2D.LinecastAll(slicePoints[0], slicePoints[1]).ToList();
@@ -98,14 +102,14 @@ public class PlayerControls : MonoBehaviour
                 continue;
             }
             
-            // Slice fruit
+            // Slice food
             Transform parentFood = maskCollider.transform.parent;
 
             // Gets spawn point
             Vector2 sliceEdgePoint_0 = Physics2D.Raycast(slicePoints[0], (slicePoints[1] - slicePoints[0]).normalized, 100).point;
             Vector2 sliceEdgePoint_1 = Physics2D.Raycast(slicePoints[1], (slicePoints[0] - slicePoints[1]).normalized, 100).point;
-            Debug.DrawLine(slicePoints[0], sliceEdgePoint_0, Color.blue, 100f);
-            Debug.DrawLine(slicePoints[1], sliceEdgePoint_1, Color.blue, 100f);
+            // Debug.DrawLine(slicePoints[0], sliceEdgePoint_0, Color.blue, 100f);
+            // Debug.DrawLine(slicePoints[1], sliceEdgePoint_1, Color.blue, 100f);
             Vector2 sliceCenter = (sliceEdgePoint_0 + sliceEdgePoint_1) / 2f;
 
             // Rotate mask to be parallel to slice
@@ -116,41 +120,15 @@ public class PlayerControls : MonoBehaviour
 
             // Picks one side of the slice for the mask to go to
             Vector2 perpendicularSlice = Vector2.Perpendicular(slicePoints[0]-slicePoints[1]).normalized;
-            // float sliceDistance = Vector2.Distance(sliceEdgePoint_0, sliceEdgePoint_1);
-            // Debug.Log(sliceDistance);
 
             // Spawn Mask
             Vector2 spawnPos = sliceCenter + spriteMask.transform.localScale.x /2f * perpendicularSlice;
             Transform currentSpriteMask = null;
-            // foreach (Transform sm in parentFruit.GetChild(1))
-            // {
-            //     if (!sm.gameObject.activeSelf) {
-            //         currentSpriteMask = sm;
-            //         currentSpriteMask.gameObject.SetActive(true);
-            //         currentSpriteMask.transform.SetPositionAndRotation(spawnPos, Quaternion.Euler(0,0,rotAng));
-            //         break;
-            //     }
-            // }
-            // if (currentSpriteMask == null) {
-                currentSpriteMask = Instantiate(spriteMask, spawnPos, Quaternion.Euler(0,0,rotAng), parentFood.GetChild(1)).transform;
-            // }
+            currentSpriteMask = Instantiate(spriteMask, spawnPos, Quaternion.Euler(0,0,rotAng), parentFood.GetChild(1)).transform;
 
             // Create other side slice
             GameObject otherSlice = null;
-            // foreach (Transform food in parentFruit.parent)
-            // {
-            //     if (!food.gameObject.activeSelf) {
-            //         otherSlice = food.gameObject;
-            //         Debug.Log(otherSlice.name);
-            //         otherSlice.SetActive(true);
-            //         otherSlice.transform.SetPositionAndRotation(parentFruit.position, parentFruit.rotation);
-            //         otherSlice.transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
-            //         break;
-            //     }
-            // }
-            // if (otherSlice == null) {
-                otherSlice = Instantiate(parentFood.gameObject, parentFood.position, parentFood.rotation, parentFood.parent);
-            // }
+            otherSlice = Instantiate(parentFood.gameObject, parentFood.position, parentFood.rotation, parentFood.parent);
             
             float separationSpace = 0.05f;
             otherSlice.transform.GetChild(1).GetChild(otherSlice.transform.GetChild(1).childCount-1).transform.position = sliceCenter - spriteMask.transform.localScale.x / 2f * perpendicularSlice;
@@ -158,7 +136,7 @@ public class PlayerControls : MonoBehaviour
             parentFood.Translate(-perpendicularSlice * separationSpace);
             otherSlice.transform.Translate(perpendicularSlice * separationSpace);
         }
-        Debug.DrawLine(slicePoints[0], slicePoints[1], Color.black, 10f);
+        // Debug.DrawLine(slicePoints[0], slicePoints[1], Color.black, 10f);
         ResetSlicePoints();
     }
 }
