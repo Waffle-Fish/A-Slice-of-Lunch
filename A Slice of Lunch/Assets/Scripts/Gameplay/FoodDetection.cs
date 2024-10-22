a71a988d5ebcc98736aea2c18c951f439a0116f1
Luke Arcebal <103071251+LukeBArcebal@users.noreply.github.com>
2024-10-23 04:03:29 +1000

    implement food fully in box detection

--- A Slice of Lunch/Assets/Scripts/Gameplay/FoodDetection.cs
+++ A Slice of Lunch/Assets/Scripts/Gameplay/FoodDetection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FoodDetection : MonoBehaviour
 {
@@ -18,11 +19,29 @@ public class FoodDetection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    void OnTriggeerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
-        Debug.Log("Caca Mega DOO DOO");
+        Debug.Log(col.transform.parent.name + " is fully in box: " + IsFoodInBox(col));
+        //IsFoodInBox(col);
+    }
+
+    bool IsFoodInBox(Collider2D col)
+    {
+        //if (col is not PolygonCollider2D) {return false;}
+
+        //Debug.Log("bounds: "+container.bounds);
+        PolygonCollider2D col_p = (PolygonCollider2D) col;
+        foreach (Vector2 point in col_p.points)
+        {
+            Vector3 col_global_point = col.transform.parent.position + (Vector3) point /transform.lossyScale.x;
+            //Debug.Log(point);
+            if (!container.bounds.Contains(col_global_point))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
